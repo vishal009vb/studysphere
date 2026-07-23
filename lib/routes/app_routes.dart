8e4e0cde7b75f650945462c44e9cb746012a906d
Vishal <vbhoi4513@gmail.com>
2026-07-23 21:29:31 +0530

    Set splash screen image display duration to 1.5 seconds

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -235,8 +235,8 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
         FlutterNativeSplash.remove();
       });
 
-      // Fast splash display (150ms = 0.15s) for instant launch
-      await Future.delayed(const Duration(milliseconds: 150));
+      // Display user image splash screen for 1.5 seconds (1500ms)
+      await Future.delayed(const Duration(milliseconds: 1500));
 
       // Await auth state stream to resolve logged in status
       final user = await ref.read(authStateProvider.future);
